ac7ad42c7bbff8a371a82f4db474ee14d7532adf
YESSER TECH <aidenyassin115@gmail.com>
2024-12-17 14:13:03 +0300

    Add files via upload

---
+++ Commands/repos.js
@@ -0,0 +1,46 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+
+zokou({ nomCom: "repo", catégorie:"Général", reaction: "⚙️", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+  const githubRepo = 'https://api.github.com/repos/bmbttz/LUKAS-MD';
+  const img = 'https://files.catbox.moe/3kdkf6.jpg';
+
+  try {
+    const response = await fetch(githubRepo);
+    const data = await response.json();
+
+    if (data) {
+      const repoInfo = {
+        stars: data.stargazers_count,
+        forks: data.forks_count,
+        lastUpdate: data.updated_at,
+        owner: data.owner.login,
+      };
+
+      const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
+      const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
+
+      const gitdata = `HELLO WHATSAPP USER
+THIS IS LUKAS-MD .\n SUPPORT OUR CHANNEL BY VIEW AND FOLLOW, https://whatsapp.com/channel/0029VakA1mu35fM18opH1s30
+
+╭─────────────────────➳
+│╭────────────────────➳
+││ 🗼 *REPOSITORY:* ${data.html_url}
+││ 🌟 *STARS:* ${repoInfo.stars}
+││ 🧧 *FORKS:* ${repoInfo.forks}
+││ 📅 *RELEASE DATE:* ${releaseDate}
+││🕐 *UPDATE ON:* ${repoInfo.lastUpdate}
+││ 👨‍💻 *OWNER:* *king lukas*
+││ 💞 *THEME:* *lukas*
+││ 🥰 *LUKAS-MD*
+│╰────────────────────➳`;
+
+      await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
+    } else {
+      console.log("Could not fetch data");
+    }
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+});
